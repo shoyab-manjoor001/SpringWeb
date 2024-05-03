f1cb3e7a075a86eab9d21f5c2fa9692de72fd5d9
Shoyab <s.manjoor001@gmail.com>
2024-05-03 17:40:29 +0530

    added Spring MVC Project

---
+++ SpringMvcEx/src/test/java/com/app/SpringMvcEx/SpringMvcExApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.SpringMvcEx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcExApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
